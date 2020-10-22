d4deab8d9cf1b9c3456691a34041ed43f1a35dad
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-22 11:57:22 +0800

    Update results.js

--- pages/results.js
+++ pages/results.js
@@ -51,6 +51,18 @@ function numberCorrect(item, index, array) {
   return;
 }
 
+function uniq(a) {
+  var prims = {"boolean":{}, "number":{}, "string":{}}, objs = [];
+
+  return a.filter(function(item) {
+      var type = typeof item;
+      if(type in prims)
+          return prims[type].hasOwnProperty(item) ? false : (prims[type][item] = true);
+      else
+          return objs.indexOf(item) >= 0 ? false : objs.push(item);
+  });
+}
+
 let juniorTeamChallengeScoresProcessed = orderBy(
   juniorTeamChallengeScores,
   "Points",
