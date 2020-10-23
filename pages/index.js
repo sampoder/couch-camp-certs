79e46fa66ff00a50b48eac6a7577786dd5c6c241
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-23 11:19:15 +0800

    Add a little image

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,20 @@
+import Head from "next/head";
+
+const Meta = ({ name = "Couch Camp 2", title = "Certificates" }) => (
+  <Head>
+    <title>
+      {title} - {name}
+    </title>
+    <meta property="og:title" content={title} />
+    <meta name="twitter:title" content={title} />
+    <meta property="og:type" content="website" />
+    <meta property="og:site_name" content={name} />
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="msapplication-TileColor" content="#DD5B39" />
+    <meta name="theme-color" content="#DD5B39" />
+  </Head>
+);
+
 const Home = (props) => (
   <div
     style={{
@@ -7,6 +24,11 @@ const Home = (props) => (
       paddingTop: "30px",
     }}
   >
+    <Meta />
+    <img
+      src="https://cloud-57pt0xtsf.vercel.app/0cclogo.png"
+      style={{ margin: "20px", height: "100px" }}
+    />
     Hey there scholar! Please check your email to find your certificate. Thanks
     for joining us for Couch Camp :D
   </div>
