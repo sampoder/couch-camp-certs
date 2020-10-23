e87ad0b0d0e28c00df808d382da2baf4f4c69d8e
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-10-23 11:15:47 +0800

    Add blank home page

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+const Home = (props) => (
+  <div
+    style={{
+      minHeight: "100vh",
+      textAlign: "center",
+      padding: "10px",
+      paddingTop: "30px",
+    }}
+  >
+    Hey there scholar! Please check your email to find your certificate. Thanks
+    for joining us for Couch Camp :D
+  </div>
+);
+
+export default Home;
